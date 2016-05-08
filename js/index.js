e67eb8d15a9c23409c88615995c5d59fe94a3252
Raymond Chung <raymond_chung2@hotmail.com>
2016-05-08 00:16:01 -0400

    Still adding

--- js/index.js
+++ js/index.js
@@ -2,4 +2,13 @@ $(".button-collapse").sideNav();
 
 $(document).ready(function(){
     $('.scrollspy').scrollSpy();
+    $('ul.tabs').tabs();
   });
+
+$(window).scroll(function() {
+	if ($(".navbar").offset().top > 50) {
+        $(".custom").addClass("custom2");
+    } else {
+        $(".custom").removeClass("custom2");
+    }
+})
