f17831a8c50b26d76bb5d5a1e14195c766ca6a73
Raymond Chung <raymond_chung2@hotmail.com>
2016-05-20 14:03:57 -0400

    Need to change links to social icons and use jekyll

--- js/index.js
+++ js/index.js
@@ -12,3 +12,9 @@ $(window).scroll(function() {
         $(".custom").removeClass("custom2");
     }
 })
+
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
+       
